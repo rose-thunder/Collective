267f4b1a2dff5323ace65610079a6a5bf33d69e3
Elli Cano <123842690+crazyelli@users.noreply.github.com>
2024-04-15 17:47:18 -0700

    changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("expected to read line");
+    
+    let int_input: i64 = input.trim().parse().unwrap();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read line");
-    println!("{}", input);
+    println!("{}", int_input)
 }

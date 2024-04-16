8015266b0c4cca5f6e95fa7ec47811320fec8979
Elli Cano <123842690+crazyelli@users.noreply.github.com>
2024-04-15 17:28:25 -0700

    changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::io;
+
 fn main() {
-    const SECONDS_IN_MINUTE: u32 = 60;
-    println!("{}", SECONDS_IN_MINUTE);
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read line");
+    println!("{}", input);
 }

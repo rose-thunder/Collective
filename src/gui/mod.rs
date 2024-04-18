4cccf8517dfe086d738d88848a93439505ea7269
Elli Cano <123842690+crazyelli@users.noreply.github.com>
2024-04-17 19:41:30 -0700

    fixed removed code

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,29 +2,6 @@ pub mod style;
 pub mod views;
 pub mod widgets;
 
-use iced::{Application, Command, Element, Theme};
+use iced::{Sandbox, Command, Element, Theme};
 
-pub struct CollectiveGui;
 
-impl Application for CollectiveGui {
-    type Theme = Theme;
-    type Executor = iced::executor::Default;
-    type Flags = ();
-    type Message = ();
-
-    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (CollectiveGui, Command::none())
-    }
-
-    fn title(&self) -> String {
-        String::from("Collective")
-    }
-
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
-    }
-
-    fn view(&self) -> Element<'_, Self::Message, Self::Theme, iced::Renderer> {
-        "Welcome to Collective".into()
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
-use iced::{Application, Settings};
+use iced::{Sandbox, Settings};
 
 mod app;
 mod gui;
-
-pub fn main() -> iced::Result {
-    gui::CollectiveGui::run(Settings {
-        window: iced::window::Settings {
-            size: iced::Size::new(1920.0, 1080.0),
-            resizable: true,
-            decorations: true,
-            ..iced::window::Settings::default()
-        },
-        default_text_size: iced::Pixels(18.0),
-        ..Settings::default()
-    })
-}

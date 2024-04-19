05e9b01beaea123130053cd1fda52025828e7a6b
Elli Cano <123842690+crazyelli@users.noreply.github.com>
2024-04-18 18:42:21 -0700

    base gui module code

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,12 +2,11 @@ pub mod style;
 pub mod views;
 pub mod widgets;
 
-use iced::{Application, Command, Element, Theme};
+use iced::{Alignment, Application, Command, Element, Length, Renderer, Theme};
 
 #[derive(Default, Debug, Clone)]
 enum View {
     #[default]
-    About,
     File,
     Tags,
     Settings,
@@ -25,12 +24,10 @@ pub struct CollectiveGui {
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    AboutPressed,
     SettingsPressed,
     TagsPressed,
     FilePressed,
     TagsAction(TagsMessage),
-    AboutAction(AboutMessage),
     SettingsAction(SettingsMessage),
     FileAction(FileMessage),
     LoadFile(Vec<Folder>),
@@ -48,12 +45,12 @@ impl Application for CollectiveGui {
             Command::batch([Command::perform(get_file_list(), Message::LoadFile)]),
         )
     }
-    
+
     fn title(&self) -> String {
         String::from("Collective")
     }
 
-    fn update(&mut self, msg:Message) -> Command<Self::Message> {
+    fn update(&mut self, msg: Message) -> Command<Self::Message> {
         match msg {
             #[allow(clippy::option_if_let_else)]
             Message::LoadFile(file_list) => {
@@ -61,7 +58,7 @@ impl Application for CollectiveGui {
                     Some(s_file) => {
                         // try to reload last selected file
                         file_list.iter().cloned()
-                    },
+                    }
                     None => file_list.first().cloned(),
                 };
                 self.file_list = file_list;
@@ -71,20 +68,53 @@ impl Application for CollectiveGui {
                     self.update(Message::SettingsAction(SettingsMessage));
                 }
 
-                self.update(Message::TagsAction(TagsMessage::LoadTagList(true))}
+                self.update(Message::TagsAction(TagsMessage::LoadTagList(true)))
+            }
             Message::TagsPressed => {
                 self.view = View::Tags;
-                Command::none()
-            }
-            Message::AboutPressed => {
-                self.view = View::About;
                 Command::none()
             }
             Message::SettingsPressed => {
                 self.view = View::Settings;
                 Command::none()
             }
+            Message::FilePressed => {
+                self.view = View::File;
+                Command::none()
+            }
+            Message::TagsAction(msg) => self
+                .tags_view
+                .update(&mut self.settings_view, msg)
+                .map(Message::TagsAction),
+            Message::SettingsAction(msg) => self
+                .settings_view
+                .update(&mut self.settings_view, msg)
+                .map(Message::SettingsAction),
+            Message::FileAction(msg) => self
+                .file_view
+                .update(&mut self.settings_view, msg)
+                .map(Message::FileAction),
         }
+    }
+
+    fn view(&self) -> Element<'_, Self::Message, Self::Theme, iced::Renderer> {
+        let navigation_container =
+            nav_menu(&self.file_list, self.selected_file.clone(), &self.tags_view);
+
+        let selected_file = self.selected_file.clone().unwrap_or_default();
+        let main_container = match self.view {
+            View::Tags => self
+                .tags_view
+                .view(&self.settings_view, &selected_file)
+                .map(Message::TagsAction),
+            View::Settings => self.settings_view.view().map(Message::SettingsAction),
+            View::File => self.file_view.view(&selected_file).map(Message::FileAction),
+        };
+
+        column![navigation_container, main_container]
+            .width(Length::Fill)
+            .align_items(Alignment::Center)
+            .into()
     }
 }
 
